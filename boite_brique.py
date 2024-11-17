86c6a0fa2db3035b9ae4f53afee20f405979d97a
frank4466 <frank.sauret@gmail.com>
2024-11-17 12:00:47 +0100

    Maj Trad

--- boite_brique.py
+++ boite_brique.py
@@ -43,7 +43,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #  0.2 changes to unittouu for Inkscape 0.91
 #  0.1 February 2011 - basic lasercut box with dimples etc
 
-__version__ = "2024.1"
+__version__ = "2024.2"
 
 #from math import *
 import sys
@@ -111,7 +111,7 @@ class LasercutBox(inkex.Effect):
                         help="Are kerf define by material")
         self.arg_parser.add_argument("-o", "--materiaux",
                         type=float,
-                        dest="materiaux", default=1, #**********************************************
+                        dest="materiaux", default=0.15,
                         help="Kerf size define by material")
         self.arg_parser.add_argument("-k", "--kerf_size",
                         type=float,
